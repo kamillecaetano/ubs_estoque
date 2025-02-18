ab0b02b1c84ff89179f464b81f94133d85f4b0b8
Kamille Santos <kamillecaetano93@gmail.com>
2025-02-18 08:18:12 -0300

    Alterações no backend

--- backend/index.js
+++ backend/index.js
@@ -22,6 +22,43 @@ server.get('/', (req, res) => {
   res.send('Sistema de Estoque da UBS');
 });
 
+server.post('/registro', async (req, res) => {
+  const {nome, email, telefone, senha} = req.body;
+  try{
+    const {rows} = await pool.query(
+      'INSERT INTO usuarios (nome, email, telefone, senha) VALUES ($1, $2, $3, $4) RETURNING *',
+      [nome, email, telefone, senha]
+    );
+    res.status(201).json(rows[0]);
+  }catch(err){
+    console.error(err.message);
+    res.status(500).send('Erro no servidor');
+  }
+});
+
+server.post('/login', async (req, res) => {
+  const {email, senha} = req.body;
+  try{
+    const {rows} = await pool.query(
+      'SELECT * FROM usuarios WHERE email = $1', [email]
+    );
+    if (rows.length === 0) {
+      return res.status(401).json({ erro: 'E-mail incorreto' });
+    }
+
+    const usuario = rows[0];
+
+    if (senha !== usuario.senha) {
+      return res.status(401).json({ erro: 'Senha incorreta' });
+    }
+
+    res.status(200).json({ mensagem: 'Login bem-sucedido'});
+  }catch(err){
+    console.error(err.message);
+    res.status(500).send('Erro no servidor');
+  }
+});
+
 server.get('/medicamentos', async (req, res) => {
   try{
     const {rows} = await pool.query('SELECT * FROM medicamentos');
@@ -79,5 +116,14 @@ server.delete('/medicamentos/:id', async (req, res) => {
     res.status(500).send('Erro no servidor');
   }
 });
+
+
+
+
+
+
+
+
+
 
 server.listen(port);
